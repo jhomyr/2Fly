702c5a7a96d896609eebfec56bc7c039ad79b256
Kilo <madeforkarl16@gmail.com>
2025-04-27 22:14:47 +0800

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,1 +1,21 @@
+package com.mycompany.2Fly;
+import java.time.*
+
+  public class Flight {
+  private String flightType, destination;
+  private localTime departureTime, arrivalTime;
+  private double airlineFare;
+  private Seats[] seats;
+
+  public Flight(String flightType, String destination, LocalDateTime departureTime,
+                  LocalDateTime arrivalTime, double ticketFare, Seats[] seats) {
+        this.flightType = flightType;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.airlineFare = airlineFare;
+        this.seats = seats;
+    }
+
+}
 
