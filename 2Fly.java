111aebc091e8324e68d30cf2853bcb00b0df29ac
Theresa Cañazares <canazarestheresa@gmail.com>
2025-05-09 21:36:51 +0800

    2Fly.java
    Signed-off-by: Theresa Cañazares <canazarestheresa@gmail.com>

--- 2Fly.java
+++ 2Fly.java
@@ -1,1 +1,18 @@
+    public static void viewReservation() {
+        if (currentReservation == null) {
+            System.out.println("No reservations available.");
+            return;
+        }
 
+        System.out.print("Enter your Booking ID: ");
+        String existingID = scanner.nextLine();
+
+        if (currentReservation.getBookingID().equalsIgnoreCase(existingID)) {
+            System.out.println("\n--- Reservation Found ---");
+            currentReservation.printReceipt();
+            System.out.println("\n--- End of Reservation Details ---");
+        } else {
+            System.out.println("Reservation with Booking ID '" + existingID + "' does not exist.");
+        }
+    }
+//end sa view
